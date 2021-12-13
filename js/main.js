4d4c3d1413591ac74f884d0d387ae9c134102c3f
Andrey <maoltin@gmail.com>
2021-12-13 14:46:17 +0300

    Added 4th lession and extra task

--- js/main.js
+++ js/main.js
@@ -1,14 +1,54 @@
 $(document).ready(function() {
-  console.log("сайт готов к манипуляциям");
   var currentFloor = 2;
+  var currentFlat = 1;
+  let usCurrentFlat = 1;
   var floorPath = $(".home-image path");
   var counterUp = $(".counter-up");
   var counterDown = $(".counter-down");
+  var modal = $(".modal");
+  var modalCloseButton = $(".modal-close-button");
+  var viewFlatsButton = $(".view-flats");
+  var flatPath = $(".modal-dialog path");
+  let flatLink = $(".flat-item a");
+
+  // Квартиры
+  flatPath.on("mouseover", function() {
+    let currentFlatOrder = +$(this).attr("data-flat") - 1;
+    
+    // $('.flat-link').removeClass('floor-link-mouseover');
+    $(`.flat-link:eq(${currentFlatOrder})`).addClass("flat-link-mouseover");
+    // console.log($(`.flat-link:eq(${currentFlatOrder})`));
+  });
+  flatPath.on("mouseout", function() {
+    $('.flat-link').removeClass('flat-link-mouseover');
+  });
+  
+  // $(".flat-link").on("mouseover", () => {
+  //   console.log($(this).index());
+  // });
+  
+  flatLink.on("mouseover", function() {
+    currentFlat = +$(this).attr("data-flat-link");
+    let usCurrentFlat = currentFlat.toLocaleString('en-US', {minimumIntegerDigits: 2,
+      useGroupping: false });
+    $(`[data-flat=${usCurrentFlat}]`).addClass("flat-path-mouseover");
+  });
+  flatLink.on("mouseout", function() {
+    $(`.flats path`).removeClass("flat-path-mouseover");
+  });
+
+  // Этажи
   floorPath.on("mouseover", function() {
     floorPath.removeClass("current-floor");
     currentFloor = $(this).attr("data-floor");
+    console.log(this)
     $(".counter").text(currentFloor);
   });
+
+  floorPath.on("click", toggleModal);
+  modalCloseButton.on("click", toggleModal);
+  viewFlatsButton.on("click", toggleModal);
+
   counterUp.on("click", function() {
     if (currentFloor < 18) {
       currentFloor++;
@@ -29,4 +69,7 @@ $(document).ready(function() {
       $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
     }
   });
+  function toggleModal() {
+    modal.toggleClass("is-open");
+  }
 });
